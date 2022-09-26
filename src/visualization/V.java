55ce24af15403346e17b67b6f6a451868e1d4b3b
nightmeo <1295304788@qq.com>
2022-09-26 21:00:45 +0800

    0

--- src/visualization/V.java
+++ src/visualization/V.java
@@ -142,7 +142,7 @@ public class V {
                     drawArea.repaint();
                 },
                 e -> {
-                    final int tracks = 12;
+                    final int tracks = 8;
 
                     final double c = 20.0;
 
@@ -152,7 +152,7 @@ public class V {
 
                     final double la = 200;
 
-                    final double tl = 600.0;
+                    final double tl = 800.0;
 
                     if (isPause) {
                         drawArea.repaint();
